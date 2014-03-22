beda5b556e156094a7b9afbc0eda8ed9b71768ad
kstoriadis <federico.gonzalezc@gmail.com>
2014-03-22 18:16:36 -0300

    Con delay del home
    Ahora se incluye un delay en el hover de las cajas del home, se modifico
    el css de contenidos para que el agrandador de letra esté en el cuerpoy
    se limito el alto del contenedor de links

---
+++ js/cyt.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+	var navegacion = {};	
+
+	navegacion["cyt"] = "pages/cyt/cytprincipal.html";
+	navegacion["cytini"] = "pages/cyt/inicial.html";
+	navegacion['cytsupcli'] = "pages/cyt/superclinica.html";
+	navegacion['cytfaciauto'] = "pages/cyt/facilitadores.html";
+	navegacion["cytsensi"] = "pages/cyt/sensibilizacion.html";
+
+	$('#cuerpoContenido > p').css("font-size", $('#tamanioLetra').slider("option", "value"));
+
+	$('#titulo').click(function() {
+
+			var linkS = $('.linkSeleccionado');
+
+			$(linkS).removeClass("linkSeleccionado");
+			$(this).addClass("linkSeleccionado");
+			linkS = this;
+
+			$('#cuerpo').fadeOut('slow', function() {
+				$('#cuerpo > #tituloSeccion').html($(linkS).html());
+				
+				$.get(navegacion[$(linkS).attr("rel")], function(msg) {
+					$('#cuerpoContenido').html(msg);
+					$('#cuerpo').fadeIn('slow');
+					//tomar tamaño letra elegido
+					$('#cuerpoContenido > p').css("font-size", $('#tamanioLetra').slider("option", "value"));
+					var altura1 = $("html").height() - $("#header").height() - $("#footer").height() - 5;
+					//el 5 es por el padding
+					var altura2 = $("#cuerpo").height() * 1.1;
+					//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+					$("#wrapper").height(altura1 < altura2 ? altura2 : altura1);					
+					
+					//$('#cuerpoContenido').html('CAMBIO');
+					
+				});
+				
+			});
+		}); 
+
+	$('#linksNavegacion > ul > .linkNavegacion').each(function(index) {
+		$(this).click(function() {
+
+			var linkS = $('.linkSeleccionado');
+
+			$(linkS).removeClass("linkSeleccionado");
+			$(this).addClass("linkSeleccionado");
+			linkS = this;
+
+			$('#cuerpo').fadeOut('slow', function() {
+				$('#cuerpo > #tituloSeccion').html($(linkS).html());
+				
+				$.get(navegacion[$(linkS).attr("rel")], function(msg) {
+					$('#cuerpoContenido').html(msg);
+					$('#cuerpo').fadeIn('slow');
+					//tomar tamaño letra elegido
+					$('#cuerpoContenido > p').css("font-size", $('#tamanioLetra').slider("option", "value"));
+					var altura1 = $("html").height() - $("#header").height() - $("#footer").height() - 5;
+					//el 5 es por el padding
+					var altura2 = $("#cuerpo").height() * 1.1;
+					//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+					$("#wrapper").height(altura1 < altura2 ? altura2 : altura1);					
+					
+					//$('#cuerpoContenido').html('CAMBIO');
+					
+				});
+				
+			});
+		});
+	});
+	
+
+	
+});
--- js/home.js
+++ js/home.js
@@ -1,52 +1,29 @@
 jQuery.fn.animateAuto = function(prop, speed, callback) {
 	var elem, height, heightNeg;
-	return this.each(function(i, el) { el = jQuery(el), elem = el.clone().css({
+	return this.each(function(i, el) {
+		el = jQuery(el), elem = el.clone().css({
 			"overflow" : "visible",
 			"height" : "auto",
 			"max-width" : '180px'
 		}).appendTo("body");
-		 height = elem.css("height"), heightNeg = elem.height() * 1 / -3, elem.remove();
+		height = elem.css("height"), heightNeg = elem.height() * 1 / -3, elem.remove();
 		var margin = heightNeg + 'px 0 0 0';
-	if(prop == 'both'){
-		el.animate({
-			"height" : height,
-			"margin" : margin
-		}, speed, callback);
-		}else{
+		if (prop == 'both') {
 			el.animate({
-			"height" : height
-		}, speed, callback);
+				"height" : height,
+				"margin" : margin
+			}, speed, callback);
+		} else {
+			el.animate({
+				"height" : height
+			}, speed, callback);
 		}
 
 	});
 }
 
 $(document).ready(function() {
-	$(".agrandable").hover(function() {
-		var elem, valHeight, heightNeg;
-		$(this).children('p').each(function(i, el) { el = jQuery(el), elem = el.clone().css({
-			"overflow" : "visible",
-			"height" : "auto",
-			"max-width" : '180px'
-		}).appendTo("body");
-		 valHeight = elem.height() * 1.9, heightNeg = elem.height() * 2 / -3, elem.remove();
-		
-		});
-		 var margin = heightNeg + 'px 2% 0 2%';
-		var padre = $(this); 
-		$(this).animate({height:valHeight +'px',"margin" : margin},1000);
-		$(this).children('p').animateAuto("height", 1000);
-		
-	}, function() {
-		$(this).children('p').animate({			
-			height: '35%'
-		}, 1000);
-		$(this).animate({			
-			height: '90%',
-			margin: '1% 2% 0 2%'
-		}, 1000);
-	});
-	
+
 	$('.imagenPrincipal > ul').height($(".imagenPrincipal").height());
 	$('.imagenPrincipal').unslider({
 		speed : 1000, //  The speed to animate each slide (in milliseconds)
@@ -56,6 +33,43 @@ $(document).ready(function() {
 		}, //  A function that gets called after every slide animation
 		keys : true, //  Enable keyboard (left, right) arrow shortcuts
 		dots : true, //  Display dot navigation
-		fluid : false              //  Support responsive design. May break non-responsive designs
+		fluid : false //  Support responsive design. May break non-responsive designs
+	});
+	var timer;
+	var delay = 500;
+
+	$(".agrandable").hover(function() {
+		var elLink = $(this);
+		 timer = setTimeout(function(objeto) {
+        // do your stuff here
+    
+		var elem, valHeight, heightNeg;
+		$(objeto).children('p').each(function(i, el) {
+			el = jQuery(el), elem = el.clone().css({
+				"overflow" : "visible",
+				"height" : "auto",
+				"max-width" : '180px'
+			}).appendTo("body");
+			valHeight = elem.height() * 1.8, heightNeg = elem.height() * 2 / -3, elem.remove();
+
+		});
+		var margin = heightNeg + 'px 2% 0 2%';
+		var padre = $(objeto);
+		$(objeto).animate({
+			height : valHeight + 'px',
+			"margin" : margin
+		}, 1000);
+		$(objeto).children('p').animateAuto("height", 1000);
+	}, delay, elLink);
+	}, function() {
+		clearTimeout(timer);
+		$(this).children('p').animate({
+			height : '35%'
+		}, 1000);
+		$(this).animate({
+			height : '90%',
+			margin : '1% 2% 0 2%'
+		}, 1000);
 	});
 });
+
