5ce10a28ae55b720cc504a56c93dbad421543216
kstoriadis <federico.gonzalezc@gmail.com>
2014-03-28 20:57:05 -0300

    cambios en el footer
    Pensando en proxima version para mostrar

---
+++ js/noticias.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$.get("../../header.html", function(msg) {
+		$('#header').html(msg);
+		$.get("../../footer.html", function(footCont) {
+			$('#footer').html(footCont);
+			$("#fondoHeader").height($("#header").height() - $("#menu").height() - 1);
+			var altura1 = $("html").height() - $("#header").height() - $("#footer").height() - 5;
+			//el 5 es por el padding
+			var altura2 = $("#cuerpo").height() * 1.1 + $("#cuerpoGrande").height() * 1.1;
+			//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+			$("#wrapper").height(altura1 < altura2 ? altura2 : altura1);
+			//el 5 es por el padding
+			$('#logo').click(function() {
+				window.location.href = "/itineris/home.html";
+			});
+		});
+
+	});
+
+	$('#tamanioLetra').slider({
+		min : 11,
+		max : 43,
+		value : 12,
+		step : 5,
+		change : function(event, ui) {
+			$('#cuerpoContenido > p').css("font-size", $('#tamanioLetra').slider("option", "value"));
+			$('.campoForm').css("font-size", $('#tamanioLetra').slider("option", "value"));
+			var altura1 = $("html").height() - $("#header").height() - $("#footer").height() - 5;
+			//el 5 es por el padding
+			var altura2 = $("#cuerpo").height() * 1.1 + $("#cuerpoGrande").height() * 1.1;
+			//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+			$("#wrapper").height(altura1 < altura2 ? altura2 : altura1);
+		}
+	});
+
+});
